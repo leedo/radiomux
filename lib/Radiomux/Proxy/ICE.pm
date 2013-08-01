d04ac2b3d62f4f1166686dfd0159bf90f13d6225
Lee Aylward <lee@laylward.com>
2013-08-01 16:06:08 -0400

    whoops, headers are hard

--- lib/Radiomux/Proxy/ICE.pm
+++ lib/Radiomux/Proxy/ICE.pm
@@ -16,10 +16,10 @@ class ICE extends Radiomux::Proxy {
     tcp_connect $uri->host, $uri->port, sub {
       my ($fh) = @_ or die "unable to connect to stream";
       my $h = AnyEvent::Handle->new(fh => $fh);
-      $h->push_write("GET " . $uri->path . " HTTP/1.0\012\015\012\015");
+      $h->push_write("GET " . $uri->path . " HTTP/1.0\015\012\015\012");
       my $headers = [];
-      $h->push_read(regex => "\012\015", sub {
-        for my $header (split "\r\n", $_[1]) {
+      $h->push_read(regex => qr{\015\012\015\012}, sub {
+        for my $header (split "\015\012", $_[1]) {
           if ($header =~ /^([a-zA-Z_-]+):\s*(.+)/) {
             push @$headers, $1, $2;
           }
